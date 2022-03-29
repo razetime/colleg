d6285c4d9c9ae924b134f3221c146ea593e924bd
razetime <raghuallthetime@hotmail.com>
2022-03-29 21:38:15 +0530

    things

--- CD/leadtrail.c
+++ CD/leadtrail.c
@@ -8,22 +8,53 @@ int find(char* str, char ch){
     }
 }
 
-void leading(char sym, char prods[][50], int pn, char* nt, int* L) {
-    int ntn=strlen(nt);
-    for(int i=0; i<2; i++){
-        printf("%i ", L[i]);
+void pa(int arr[], int n) {
+    puts("");
+    for(int i=0; i<n; i++) {
+        printf("%i ", arr[i]);
     }
     puts("");
+}
+
+void leading(char sym, char prods[][50], int pn, char* nt, int* L) {
+    int ntn=strlen(nt);
+    //printf("%i %c\n", pn, sym);
     for(int i=0; i<pn; i++) {
         if(sym == prods[i][0]){
+            //puts("MAtched");
             char c = prods[i][3];
             if(isupper(c)){
-                leading(c,prods,pn,nt,L);
+                if(c != prods[i][0]) leading(c,prods,pn,nt,L);
+                //pa(L, 50);
                 if(!isupper(prods[i][4])) {
-                    printf("doing thing for: %c %i\n", prods[i][4], find(nt, prods[i][4]));
+                    //printf("doing thing for: %s %c %i\n", nt, prods[i][4], find(nt, prods[i][4]));
                     L[find(nt, prods[i][4])] = 1;
+                    //pa(L, 50);
+                }
+                //printf("Printing");
+               // pa(L, 50);
+            } else {
+                L[find(nt, c)] = 1;
+            }
+        }
+    }
+}
+
+void trailing(char sym, char prods[][50], int pn, char* nt, int* L) {
+    int ntn=strlen(nt);
+    for(int i=0; i<pn; i++) {
+        if(sym == prods[i][0]){
+            int l = strlen(prods[i])-1;
+            char c = prods[i][l];
+            if(isupper(c)){
+                if(c != prods[i][0]) trailing(c,prods,pn,nt,L);
+                if(!isupper(prods[i][l-1])) {
+                    //printf("\ndoin your mom %c", prods[i][l-1]);
+                    L[find(nt, prods[i][l-1])] = 1;
+                    //pa(L, 50);
                 }
             } else {
+                //printf("\ndoin your mom %c", c);
                 L[find(nt, c)] = 1;
             }
         }
@@ -47,14 +78,23 @@ int main() {
 
     
     int L[50];
-    
+    for(int i=0; i<50; i++) L[i]=0;
     puts("enter symbol");
     char s;
     scanf(" %c", &s);
-    leading(s, prods, n, nt,  L);
+    //printf("Char: %c\n", s);
+    leading(s, prods, n, t,  L);
     printf("Leading(%c) -> ", s);
     for(int i=0; i<strlen(t); i++) {
         if(L[i]) putchar(t[i]);
+    }
+
+    int TL[50];
+    for(int i=0; i<50; i++) TL[i]=0;
+    trailing(s, prods, n, t,  TL);
+    printf("\nTrailing(%c) -> ", s);
+    for(int i=0; i<strlen(t); i++) {
+        if(TL[i]) putchar(t[i]);
     }
     puts("");
 }
